3f921c95a24cabe9c72e4b14de29516136596593
jhery-z <herimaill4@gmail.com>
2022-09-07 05:00:05 +0700

    responsive size changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -50,8 +50,8 @@ const Navbar = () => {
       style={{ backgroundColor: `${navBg}` }}
       className={
         shadow
-          ? 'fixed w-full px-28 h-20 shadow-xl z-[100] ease-in-out duration-300'
-          : 'fixed w-full px-28 h-20 z-[100]'
+          ? 'fixed w-full lg:px-28 h-20 shadow-xl z-[100] ease-in-out duration-300'
+          : 'fixed w-full lg:px-28 h-20 z-[100]'
       }
     >
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
